475fe46184ad47052ba71521167ebdb928e71983
Arty <chapurinavaleria@yandex.ru>
2025-12-18 01:23:13 +0300

    забыла git add написать..

--- src/main/java/com/mipt/valeriachapurina/reflections_and_annotations/ValidationResult.java
+++ src/main/java/com/mipt/valeriachapurina/reflections_and_annotations/ValidationResult.java
@@ -1,4 +1,32 @@
 package com.mipt.valeriachapurina.reflections_and_annotations;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ValidationResult {
+  private boolean isValid;
+  private final List<String> errors;
+
+  public ValidationResult() {
+    this.isValid = true;
+    this.errors = new ArrayList<>();
+  }
+
+  public ValidationResult(boolean isValid, List<String> errors) {
+    this.isValid = isValid;
+    this.errors = errors;
+  }
+
+  public void addError(String errorMessage) {
+    this.errors.add(errorMessage);
+    this.isValid = false;
+  }
+
+  public List<String> getErrors() {
+    return errors;
+  }
+
+  public boolean isValid() {
+    return isValid;
+  }
 }
